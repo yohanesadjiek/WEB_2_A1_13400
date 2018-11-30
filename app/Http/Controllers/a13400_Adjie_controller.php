2adbd80927c17a7ca280668dca58ffb21082ff56
unknown <kurniawanadjie8@gmail.com>
2018-11-30 11:07:33 +0700

    responsi

---
+++ app/Http/Controllers/a13400_Adjie_controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class a13400_Adjie_controller extends Controller
+{
+    //
+}
---
+++ app/a13400_Adjie_model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class a13400_Adjie_model extends Model
+{
+    //
+}
